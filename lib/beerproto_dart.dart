1f712faad6bdfe03d297de9b68684ab2845db63b
Ross Merrigan <email@rossmerrigan.com>
2023-08-30 12:57:28 +0200

    fixed readme

--- lib/beerproto_dart.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
-
-export 'src/beerproto_dart_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
