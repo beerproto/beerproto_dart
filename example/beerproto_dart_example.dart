ca3d4c5569d60076195148d0f2ab18f428164fef
Ross Merrigan <email@rossmerrigan.com>
2023-08-30 12:06:48 +0200

    init

---
+++ example/beerproto_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:beerproto_dart/beerproto_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/beerproto_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/beerproto_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
